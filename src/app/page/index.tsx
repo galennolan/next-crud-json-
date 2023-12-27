9d32fdfdb9d83c419d774c10ffe68124fa2c3ca7
galennolan <galen.nolan1@gmail.com>
2023-12-27 23:10:24 +0700

    crud-product-dengan-modal-pakedaisyui-json

---
+++ src/app/page/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+const Home: NextPage = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/app/users/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">Hello daisyUI!</button>
+    </div>
+  );
+};
+
+export default page;
